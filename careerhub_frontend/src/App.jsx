d68a682c94d9e19cba80f709598042afb2405de9
Your Name <your-email@example.com>
2025-02-19 11:15:01 +0530

    first commit

---
+++ careerhub_frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Register from './pages/register'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
