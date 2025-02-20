689a007dd2852430ab17e53b97f3cc3c04c4971f
mustafa ansari <mustafaa.edu@gmail.com>
2025-02-20 19:26:17 +0530

    c

--- careerhub_frontend/vite.config.js
+++ careerhub_frontend/vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Fix: Correct placement of 'base'
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 5173,
-  },
-  build: {
-    outDir: 'dist',
-  },
-});
+})
