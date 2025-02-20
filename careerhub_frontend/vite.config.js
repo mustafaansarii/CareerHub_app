0535880ce71573ea92f7c36bd30de4002ad4664c
mustafa ansari <mustafaa.edu@gmail.com>
2025-02-20 19:24:39 +0530

    c

--- careerhub_frontend/vite.config.js
+++ careerhub_frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   base: '/', // Fix: Correct placement of 'base'
   plugins: [react(), tailwindcss()],
   server: {
-    port: 3000,
+    port: 5173,
   },
   build: {
     outDir: 'dist',
